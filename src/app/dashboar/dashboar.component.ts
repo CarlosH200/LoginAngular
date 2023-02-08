8c15f21f30d9b9425473d1dfbd2ad3b76839f61e
CarlosH200 <carloshugop46@gmail.com>
2023-02-08 05:46:12 -0600

    Menus dashboard con ngfor

--- src/app/dashboar/dashboar.component.ts
+++ src/app/dashboar/dashboar.component.ts
@@ -6,14 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboar.component.css']
 })
 export class DashboarComponent implements OnInit {
+Modulos: any = [];
+
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(MENU);
+   
   }
-
-
+ menu = MENU;
+  
+  
   
 }
 
+const MENU = [
+  {id: 1, name:'Superman'},
+  {id: 2, name:'Batman'},
+  {id: 5, name:'BatGirl'},
+  {id: 3, name:'Robin'},
+  {id: 4, name:'Flash'}
+];
 
